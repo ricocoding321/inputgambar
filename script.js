fc6a8da88eebad1715814b75a986c45c0f955f3f
ricocoding321 <ricocoding321@gmail.com>
2025-11-28 10:48:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+document.getElementByid('imgInput').addEventlinstener('change', function (e) {
+  conts file = e.target.files[0};
+const previewImg = document.getElementByid('previewImg);
+if (file) {
+  const reader = new FileReader();
+reader.onload = function (ev) {
+}
